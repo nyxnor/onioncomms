28e4ae66db6d26181cf62a8485419892eb0b478c
nyxnor <nyxnor@protonmail.com>
2022-02-07 19:04:11 +0100

    update xmpp/prosody script and files

--- xmpp/prosody/setup.sh
+++ xmpp/prosody/setup.sh
@@ -3,6 +3,12 @@
 [ "$(id -u)" -ne 0 ] && printf '%s\n' "Run as root" && exit 1
 
 #########################
+## Notes
+##  TLS/SSL is used because some clients or server might refuse to transfer
+##  files if the transport is not authenticated, even if it is over Tor
+##  this is why https is being used
+
+#########################
 ## Variables
 requirements="tor prosody prosody-modules prosody-migrator lua-zlib openssl"
 tor_conf="/etc/tor/torrc"
@@ -21,7 +27,7 @@ for pkg in ${requirements}; do
   ## if tor was built from source and not via deb package, as well as other pkgs, find it in path
   case "${pkg}" in
     tor|prosody|openssl) command  -v "${pkg}" >/dev/null || install_pkg="${install_pkg} ${pkg}";;
-    prosody-modules|prosody-migrator|lua-zlib|*) dpkg -s "${pkg}" || install_pkg="${install_pkg} ${pkg}";;
+    prosody-modules|prosody-migrator|lua-zlib|*) dpkg -s "${pkg}" >/dev/null 2>&1 || install_pkg="${install_pkg} ${pkg}";;
   esac
 done
 # shellcheck disable=SC2086
@@ -31,6 +37,11 @@ done
 ## Onion service
 if ! grep -q "HiddenServiceDir ${tor_service_dir}" "${tor_conf}"; then
   echo "Creating Hidden Service"
+
+  ## 5222 - c2s
+  ## 5269 - s2s
+  ## 5280 - http_upload
+  ## 5281 - https_upload
   echo "
 HiddenServiceDir ${tor_service_dir}
 HiddenServiceVersion 3
@@ -49,46 +60,61 @@ hostname=$(cat ${tor_service_dir}/hostname)
 ## Prosody
 echo "Setting up prosody"
 
-cp "${script_dir}"/mod_*.lua "${prosody_lib_dir}/modules/"
-chmod 644 "${prosody_lib_dir}/modules/mod_onions.lua"
-chmod 644 "${prosody_lib_dir}/modules/mod_http_upload.lua"
-chmod 644 "${prosody_lib_dir}/modules/mod_require_otr.lua"
-chown -R root:prosody "${prosody_lib_dir}"
+## https://hg.prosody.im/prosody-modules/file/tip/mod_omemo_all_access/README.markdown
+##  0.11  Not needed, mod\_pep provides this feature already
+# cp "${script_dir}"/mod_*.lua "${prosody_lib_dir}/modules/"
+# chmod 644 "${prosody_lib_dir}/modules/mod_omemo_all_access.lua"
+# chown -R root:root "${prosody_lib_dir}"
+
+## necessary to include virtual hosts configuration files
+if ! grep -q -F "Include \"conf.d/*.cfg.lua\"" "${prosody_conf_dir}/prosody.cfg.lua"; then
+  printf '\n%s\n\n' "Include \"conf.d/*.cfg.lua\"" | tee -a "${prosody_conf_dir}/prosody.cfg.lua" >/dev/null
+fi
+chmod 640 "${prosody_conf_dir}/prosody.cfg.lua"
+test -d "${prosody_conf_dir}/conf.d" || mkdir "${prosody_conf_dir}/conf.d"
+chmod 750 "${prosody_conf_dir}/conf.d"
 
 cp "${script_dir}"/virtualhost.cfg.lua "${hostname}.cfg.lua"
 sed -i'' "s|hostname|${hostname}|g" "${hostname}.cfg.lua"
 sed -i'' "s|prosody_conf_dir|${prosody_conf_dir}|g" "${hostname}.cfg.lua"
 
-mv "${hostname}.cfg.lua" "${prosody_conf_dir}/conf.avail/${hostname}.cfg.lua"
-ln -sf "${prosody_conf_dir}/conf.avail/${hostname}.cfg.lua" "${prosody_conf_dir}/conf.d/${hostname}.cfg.lua"
+mv "${hostname}.cfg.lua" "${prosody_conf_dir}/conf.d/"
+chmod 640 "${prosody_conf_dir}/conf.d/${hostname}.cfg.lua"
 
-cp "${script_dir}"/prosody.cfg.lua "${prosody_conf_dir}/prosody.cfg.lua"
-chmod 644 "${prosody_conf_dir}/prosody.cfg.lua"
+
 chown -R root:prosody "${prosody_conf_dir}"
 
 #########################
 ## Certificate
-echo "Configuring the SSL certificate"
-#prosodyctl cert generate "${hostname}"
-cp "${script_dir}"/openssl.cnf "${script_dir}/${hostname}.openssl.cnf"
-sed -i'' "s/example.com/${hostname}/g" "${script_dir}/${hostname}.openssl.cnf"
-openssl req -new -x509 -days 1825 -nodes -out "${prosody_conf_dir}/certs/${hostname}.crt" -newkey rsa:4096 -keyout "${prosody_conf_dir}/certs/${hostname}.key" -config  "${script_dir}/${hostname}.openssl.cnf"
-rm -f "${script_dir}/${hostname}.openssl.cnf"
+if ! test -f "${prosody_var_dir}/${hostname}.crt"; then
+  echo "Configuring the SSL certificate"
+  #prosodyctl cert generate "${hostname}"
+  cp "${script_dir}"/openssl.cnf "${script_dir}/${hostname}.openssl.cnf"
+  sed -i'' "s/example.com/${hostname}/g" "${script_dir}/${hostname}.openssl.cnf"
+  openssl req -new -x509 -days 1825 -nodes -out "${prosody_conf_dir}/certs/${hostname}.crt" -newkey rsa:4096 -keyout "${prosody_conf_dir}/certs/${hostname}.key" -config  "${script_dir}/${hostname}.openssl.cnf"
+  rm -f "${script_dir}/${hostname}.openssl.cnf"
 
-ln -sf "${prosody_var_dir}/${hostname}.crt" "${prosody_conf_dir}/certs/${hostname}.crt"
-ln -sf "${prosody_var_dir}/${hostname}.key" "${prosody_conf_dir}/certs/${hostname}.key"
+  chmod 640 "${prosody_conf_dir}/certs/${hostname}.crt"
+  chmod 640 "${prosody_conf_dir}/certs/${hostname}.key"
 
-chown -R prosody:prosody "${prosody_var_dir}"
-chown -R root:prosody "${prosody_conf_dir}"
+  ln -sf "${prosody_var_dir}/${hostname}.crt" "${prosody_conf_dir}/certs/${hostname}.crt"
+  ln -sf "${prosody_var_dir}/${hostname}.key" "${prosody_conf_dir}/certs/${hostname}.key"
 
-systemctl reload prosody
+  chown -R prosody:prosody "${prosody_var_dir}"
+  chown -R root:prosody "${prosody_conf_dir}"
+
+  systemctl reload prosody
+else
+  echo "Certificate for this server already exists"
+fi
+
 
 #########################
 ## Administrator
 printf '%s\n' "Register the password for admin@${hostname}:"
 prosodyctl register admin "${hostname}"
 
-printf %s"\n
+printf %s"
 Log in with your XMPP client:
 user: admin
 hostname: ${hostname}
@@ -98,3 +124,9 @@ Verify the certificate information:
 "
 openssl x509 -in "${prosody_conf_dir}/certs/${hostname}.crt" -noout -sha256 -fingerprint
 openssl x509 -in "${prosody_conf_dir}/certs/${hostname}.crt" -noout -sha256 -dates
+
+
+#######################
+## Syntax check
+printf '\n'
+prosodyctl check config
--- xmpp/prosody/virtualhost.cfg.lua
+++ xmpp/prosody/virtualhost.cfg.lua
@@ -1,17 +1,46 @@
+https_certificate = "prosody_conf_dir/certs/hostname.crt"
+
 VirtualHost "hostname"
 
 admins = { "admin@hostname" }
-
-https_certificate = "prosody_conf_dir/certs/hostname.crt"
-
 disco_items = {
     { "conference.hostname", "Public Chatrooms" };
 }
 
 modules_enabled = {
+    -- General
+    "c2s"; -- Handle client connections
+    "s2s"; -- Handle server-to-server  connections
+    "bosh"; -- Enable BOSH clients, aka "Jabber over HTTP"
+    "http_files"; -- Serve static files from a directory over HTTP
+    "groups"; -- Shared roster support
+    "carbons"; -- Keep multiple clients in sync
+    "roster"; -- Allow users to have a roster. Recommended ;)
+    "saslauth"; -- Authentication for clients and servers. Recommended if you want to log in.
+    "tls"; -- Add support for secure TLS on c2s/s2s connections
+    "dialback"; -- s2s dialback support
+    "disco"; -- Service discovery
+    "pep"; -- Enables users to publish their avatar, mood, activity, playing music and more
+    -- "pep_simple"; -- Fallback compatibility for omemo_all_access
+
+    -- Security
     "onions"; -- Enable onion host
-    "require_otr"; -- Require Off the Record Encryption to all connections
+    --"require_otr"; -- Require Off the Record Encryption to all connections
     --"omemo_all_access"; -- Disable access control for all OMEMO related PEP nodes
+}
+
+-- in case this modules were enabled server-wide on the main prosody.cfg.lua
+-- disable them on this virtual hfor security and privacy reasons
+modules_disabled = {
+    -- Privacy
+    "version"; -- Replies to server version requests
+    "uptime"; -- Report how long server has been running
+    "time"; -- Let others know the time here on this server
+    "ping"; -- Replies to XMPP pings with pongs
+    "server_contact_info"; -- Publish contact information for this service
+
+    -- Security
+    "register"; -- Allow users to register on this server using a client and change passwords
 }
 
 onions_tor_all = true; -- pass all s2s connections through Tor
@@ -22,4 +51,37 @@ Component "upload.hostname" "http_upload"
 
 Component "conference.hostname" "muc"
     name = "Prosody Chatrooms"
-    restrict_room_creation = "true"
+    restrict_room_creation = "local" -- only users from this domain/host can create rooms
+    modules_enabled = {
+        "muc_mam";
+    }
+
+-- Disable account creation by default, for security
+-- For more information see https://prosody.im/doc/creating_accounts
+allow_registration = false
+
+-- Force clients to use encrypted connections? This option will
+-- prevent clients from authenticating unless they are using encryption.
+
+c2s_require_encryption = true
+
+-- Force servers to use encrypted connections? This option will
+-- prevent servers from authenticating unless they are using encryption.
+
+s2s_require_encryption = true
+
+-- Force certificate authentication for server-to-server connections?
+
+s2s_secure_auth = false
+
+-- Some servers have invalid or self-signed certificates. You can list
+-- remote domains here that will not be required to authenticate using
+-- certificates. They will be authenticated using DNS instead, even
+-- when s2s_secure_auth is enabled.
+
+--s2s_insecure_domains = { "insecure.example" }
+
+-- Even if you disable s2s_secure_auth, you can still require valid
+-- certificates for some domains by specifying a list here.
+
+--s2s_secure_domains = { "jabber.org" }
